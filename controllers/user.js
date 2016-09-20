908df9fd007d901bab2b3c0a13457662374b80bb
pascalhelbig <pascalhelbig@gmail.com>
2016-09-20 14:43:00 +0200

    removed some eslint errors in controllers/user.js :umbrella:

--- controllers/user.js
+++ controllers/user.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 const moment = require('moment');
 const User = require('../models/Users');
 
-const SECRET = 'SECRET!!!'
+const SECRET = 'SECRET!!!';
 
-const generateToken = user => {
+const generateToken = (user) => {
   const payload = {
     iss: 'my.domain.com',
     sub: user.id,
@@ -12,7 +12,7 @@ const generateToken = user => {
     exp: moment().add(7, 'days').unix(),
   };
   return jwt.sign(payload, SECRET);
-}
+};
 
 module.exports.signup = user =>
   new User({
@@ -21,7 +21,7 @@ module.exports.signup = user =>
     password: user.password,
   }).save()
     .then(savedUser => ({ token: generateToken(savedUser), user: savedUser.toJSON() }))
-    .catch(err => {
+    .catch((err) => {
       if (err.code === 'ER_DUP_ENTRY' || err.code === '23505') {
         throw new Error('The email address you have entered is already associated with another account.');
       }
