1e29f6e96c5398466a7757abf90c13bfc6a37365
Pascal Helbig <pascalhelbig@gmail.com>
2016-09-25 19:54:36 +0200

    added members field to GroupType

--- graphql/types/GroupType.js
+++ graphql/types/GroupType.js
@@ -1,4 +1,5 @@
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean } = require('graphql');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLBoolean, GraphQLList } = require('graphql');
+const UserType = require('./UserType');
 
 module.exports = new GraphQLObjectType({
   name: 'GroupType',
@@ -6,5 +7,6 @@ module.exports = new GraphQLObjectType({
     id: { type: GraphQLID, resolve: group => group.get('id') },
     name: { type: GraphQLString, resolve: group => group.get('name') },
     isClub: { type: GraphQLBoolean, resolve: group => group.get('is_club') },
+    members: { type: new GraphQLList(UserType), resolve: group => group.members().fetch() },
   },
 });
