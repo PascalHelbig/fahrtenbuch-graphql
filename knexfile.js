c434f0219a105ddf3f3ad738c3ff2345193e9989
Pascal Helbig <pascalhelbig@gmail.com>
2016-12-24 00:32:38 +0100

    added to envs: test and normal

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,21 @@
-module.exports = {
-  client: 'mysql',
-  connection: {
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+const envs = {
+  test: {
+    client: 'mysql',
+    connection: {
+      database: process.env.DB_DATABASE_TEST,
+      user: process.env.DB_USER_TEST,
+      password: process.env.DB_PASSWORD_TEST,
+    },
   },
-  debug: process.env.LOG_SQL === 'on',
+  normal: {
+    client: 'mysql',
+    connection: {
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    debug: process.env.LOG_SQL === 'on',
+  },
 };
+
+module.exports = envs[process.env.NODE_ENV || 'normal'];
