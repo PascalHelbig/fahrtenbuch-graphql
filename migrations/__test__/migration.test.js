6070bafafba48a12a9bbda4dffee7af47b092975
Pascal Helbig <pascalhelbig@gmail.com>
2016-12-23 23:12:02 +0100

    do a rollback in migration.test.js

--- migrations/__test__/migration.test.js
+++ migrations/__test__/migration.test.js
@@ -3,7 +3,7 @@ const config = require('../../knexfile');
 const knex = require('knex')(config);
 
 it('should migrate', () =>
-  knex.migrate.latest().finally(() =>
-    knex.destroy()
-  )
+  knex.migrate.latest()
+    .finally(() => knex.migrate.rollback())
+    .finally(() => knex.destroy())
 );
