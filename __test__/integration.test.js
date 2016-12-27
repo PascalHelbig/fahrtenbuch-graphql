ed06294c7868e4a48164bd09985977df9761b26a
Pascal Helbig <pascalhelbig@gmail.com>
2016-12-27 20:49:02 +0100

    added sign up and log in to integration test

--- __test__/integration.test.js
+++ __test__/integration.test.js
@@ -1,10 +1,11 @@
 const Promise = require('bluebird');
 const request = require('supertest-as-promised');
-const server = require('./../server');
+const server = require('../server');
 const config = require('./../knexfile');
 const knex = require('knex')(config);
 
 let app;
+let token;
 
 /**
  * start the server and migrate the database
@@ -16,16 +17,43 @@ beforeAll(() =>
   ])
 );
 
+it('should signup a new user', () => {
+  const query = `
+    mutation {
+      signup(email: "test@test.de", password: "12345") {
+        token
+      }
+    }`;
+  return request(app)
+    .post('/graphql')
+    .send({ query })
+    .expect(200);
+});
+
+it('should login the user', () => {
+  const query = `
+    mutation {
+      login(email: "test@test.de", password: "12345") {
+        token
+      }
+    }`;
+  return request(app)
+    .post('/graphql')
+    .send({ query })
+    .expect(200)
+    .then(res => JSON.parse(res.text))
+    .then((res) => {
+      token = res.data.login.token;
+      return expect(token).not.toBeNull();
+    });
+});
+
 it('should run hello world on GET /', () =>
   request(app)
     .get('/')
     .expect(200)
     .then(res => expect(res.text).toBe('Hello World!'))
 );
-
-it('should test', () => {
-// Todo: Write some graphql tests!
-});
 
 /**
  * shutdown the server and rollback the database
